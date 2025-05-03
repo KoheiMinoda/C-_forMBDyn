80a44ca869479c5db75286ec5f906153b1afbffd
KoheiMinoda <minoda.kohei@gmail.com>
2025-05-03 18:03:45 +0900

    01

--- 01practice/01-01.cpp
+++ 01practice/01-01.cpp
@@ -1,6 +1,8 @@
-#include <iostream>  // 出力に必要なヘッダファイル
+#include <iostream> // 出力に必要なヘッダファイル
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;  // 出力
-    return 0;  // 正常終了
+    std::cout << "Hello MBDyn" << std::endl; // 出力
+    std::cout << "Hello C++" << "\n"; // 出力
+    std::cout << "Hello World" << std::endl; // 出力
+    return 0; // 正常終了
 }
